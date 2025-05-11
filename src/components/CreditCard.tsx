cb66e4a3203c5e1cc9f715fc39b43b342b20cfef
jessyfady <jessyfady@icloud.com>
2025-05-11 04:27:56 +0300

    updates

--- src/components/CreditCard.tsx
+++ src/components/CreditCard.tsx
@@ -87,7 +87,7 @@ const PaymentMethod = () => {
 
         {/* Other Payment Methods */}
         <button
-          className="w-full bg-blue-200 text-blue-800 rounded-full py-2 font-medium"
+          className="w-full bg-gradient-to-br from-purple-400 to-purple-700 text-white rounded-full py-2 font-medium"
           onClick={() => setShowModal(true)}
         >
           Other payment Methods
@@ -123,7 +123,7 @@ const PaymentMethod = () => {
 
         {/* Proceed Button */}
         <button
-          className="w-full bg-blue-500 text-white py-2 rounded-full font-semibold"
+          className="w-full bg-gradient-to-br from-purple-400 to-purple-700 text-white py-2 rounded-full font-semibold"
           onClick={() => navigate("/payment-success")}
         >
           Proceed Payment
